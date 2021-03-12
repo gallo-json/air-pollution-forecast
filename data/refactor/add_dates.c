437a37454c2cd48d2d3faaa97f311f5f3afc50c4
gallo-json <gallojsantiago@gmail.com>
2021-03-12 17:06:30 -0600

    json

--- data/refactor/add_dates.c
+++ data/refactor/add_dates.c
@@ -10,23 +10,23 @@ Written in C because it was easier to do than in Python, and much faster.
 
 #define LINE_SIZE 500
 
-int main() {    
+int main[] {    
     FILE *file_ptr;
-    file_ptr = fopen("/home/jose/Programming/aiml/Data/houston-AQI-weather/data_new.csv", "r");
+    file_ptr = fopen["/home/jose/Programming/aiml/Data/houston-AQI-weather/data_new.csv", "r"];
 
     char line[LINE_SIZE];
 
     int line_num = 0;
-    while (fgets(line, LINE_SIZE, file_ptr)) {
-        int month = (line_num % 12) + 1;
-        int year = 1997 + (line_num / 12);
+    while [fgets[line, LINE_SIZE, file_ptr]] {
+        int month = [line_num % 12] + 1;
+        int year = 1997 + [line_num / 12];
 
-        if (line[0] != '\n') 
-            printf("%02d-%d", month, year); 
+        if [line[0] != '\n'] 
+            printf["%02d-%d", month, year]; 
         else 
             ++line_num;
         
-        printf("%s", line);
+        printf["%s", line];
     }
 
     return 0;
--- data/refactor/parse_past_AQI_data.py
+++ data/refactor/parse_past_AQI_data.py
@@ -4,47 +4,47 @@ import pandas as pd
 
 base_dir = "/home/jose/Programming/aiml/Data/houston-AQI-weather/"
 
-df = pd.read_csv(base_dir + "AQI_data.csv")
+df = pd.read_csv[base_dir + "AQI_data.csv"]
 
 # Remove useless columns
 del df['POC']
 del df['Flag']
 
-areas = df['Monitoring_Site'].unique()
+areas = df['Monitoring_Site'].unique[]
 
 for a in areas:
     area_df = df.loc[df['Monitoring_Site'] == a]
     area_arr = []
 
-    for row in range(len(area_df)):
-        for day in range(1, 32):
+    for row in range[len[area_df]]:
+        for day in range[1, 32]:
             date_day = f'{day:02}'
             month_year = area_df['Date'].values[row]
 
             # Different months have different number of days, so no extra NaNs
-            if (month_year[:2] == '04' or month_year[:2] == '06' or month_year[:2] == '09' or month_year[:3] == '11') and day == 31:
+            if [month_year[:2] == '04' or month_year[:2] == '06' or month_year[:2] == '09' or month_year[:3] == '11'] and day == 31:
                 break
             
             # February and leap years
             if month_year[:2] == '02':
-                if (int(month_year[3:]) % 4 == 0 and day == 30) or (int(month_year[3:]) % 4 != 0 and day == 29):
+                if [int[month_year[3:]] % 4 == 0 and day == 30] or [int[month_year[3:]] % 4 != 0 and day == 29]:
                     break
 
-            area_arr.append([date_day + '-' + month_year, area_df[str(day)].values[row]])
+            area_arr.append[[date_day + '-' + month_year, area_df[str[day]].values[row]]]
 
-    new_area_df = pd.DataFrame(area_arr, columns=['Date', 'AQI'])
+    new_area_df = pd.DataFrame[area_arr, columns=['Date', 'AQI']]
 
     # Filter out the trailing NaNs out of stations that were deactiavted prior to 02-2021
     if new_area_df['Date'].values[-1] != '28-02-2021':
-        i = len(new_area_df)
-        while pd.isna(new_area_df['AQI'].values[-1]) or new_area_df['AQI'].values[-1] == 'NV':
+        i = len[new_area_df]
+        while pd.isna[new_area_df['AQI'].values[-1]] or new_area_df['AQI'].values[-1] == 'NV':
             new_area_df = new_area_df[:-1]
             i = i - 1
     
     # File names cannot have forward slashes in name
-    a = a.replace("/", "-")
-    new_area_df.to_csv(base_dir + "stations/" + a + ".csv")
+    a = a.replace["/", "-"]
+    new_area_df.to_csv[base_dir + "stations/" + a + ".csv"]
 
-    print(a)
-    print(new_area_df.tail(4))
-    print()
+    print[a]
+    print[new_area_df.tail[4]]
+    print[]
