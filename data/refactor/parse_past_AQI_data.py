f7a1ad90d1c2185962c7dc200c7751ce5eedbec8
gallo-json <gallojsantiago@gmail.com>
2021-03-12 15:42:10 -0600

    script done

--- data/refactor/parse_past_AQI_data.py
+++ data/refactor/parse_past_AQI_data.py
@@ -1,6 +1,10 @@
+# Script to parse the original lump of AQI data and separate it into different stations
+
 import pandas as pd
 
-df = pd.read_csv("/home/jose/Programming/aiml/Data/houston-AQI-weather/AQI_data.csv")
+base_dir = "/home/jose/Programming/aiml/Data/houston-AQI-weather/"
+
+df = pd.read_csv(base_dir + "AQI_data.csv")
 
 # Remove useless columns
 del df['POC']
@@ -17,9 +21,11 @@ for a in areas:
             date_day = f'{day:02}'
             month_year = area_df['Date'].values[row]
 
+            # Different months have different number of days, so no extra NaNs
             if (month_year[:2] == '04' or month_year[:2] == '06' or month_year[:2] == '09' or month_year[:3] == '11') and day == 31:
                 break
-
+            
+            # February and leap years
             if month_year[:2] == '02':
                 if (int(month_year[3:]) % 4 == 0 and day == 30) or (int(month_year[3:]) % 4 != 0 and day == 29):
                     break
@@ -28,6 +34,17 @@ for a in areas:
 
     new_area_df = pd.DataFrame(area_arr, columns=['Date', 'AQI'])
 
+    # Filter out the trailing NaNs out of stations that were deactiavted prior to 02-2021
+    if new_area_df['Date'].values[-1] != '28-02-2021':
+        i = len(new_area_df)
+        while pd.isna(new_area_df['AQI'].values[-1]) or new_area_df['AQI'].values[-1] == 'NV':
+            new_area_df = new_area_df[:-1]
+            i = i - 1
+    
+    # File names cannot have forward slashes in name
+    a = a.replace("/", "-")
+    new_area_df.to_csv(base_dir + "stations/" + a + ".csv")
+
     print(a)
-    print(new_area_df.head(4))
+    print(new_area_df.tail(4))
     print()
