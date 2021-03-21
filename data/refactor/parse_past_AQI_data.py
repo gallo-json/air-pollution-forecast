3aa9dd49245b00f9033f7d162f98e0a718a8c350
gallo-json <gallojsantiago@gmail.com>
2021-03-21 18:49:29 -0500

    i think it finally works

--- data/refactor/parse_past_AQI_data.py
+++ data/refactor/parse_past_AQI_data.py
@@ -39,8 +39,6 @@ for a in areas:
     if new_area_df['Date'].values[-1] != '28-02-2021':
         while pd.isna(new_area_df['AQI'].values[-1]) or new_area_df['AQI'].values[-1] == 'NV' or  new_area_df['AQI'].values[-1] == 'NaN':
             new_area_df = new_area_df[:-1]
-
-    #new_area_df = new_area_df.fillna(0)
     
     # File names cannot have forward slashes in name
     a = a.replace("/", "-").strip()
--- data/refactor/parse_past_weather_data.py
+++ data/refactor/parse_past_weather_data.py
@@ -22,11 +22,10 @@ for tceq_name, tceq_coord in coords.items():
     tceq_station_df = pd.read_csv(base_dir + "stations/" + tceq_name + ".csv")
     del tceq_station_df['Unnamed: 0']
 
-    tceq_station_df['Date'] = pd.to_datetime(tceq_station_df['Date'], errors='coerce', format='%d-%m-%Y')
     first_day = tceq_station_df['Date'].values[0]
     last_day = tceq_station_df['Date'].values[-1]
 
-    missing_dates = pd.date_range(start=first_day, end=last_day).difference(tceq_station_df.Date)
+    tceq_station_df['Date'] = pd.to_datetime(tceq_station_df['Date'], errors='coerce', format='%d-%m-%Y')
 
     for label in ["air_temp", "dew_point_temp", "sea_level_pressure", "sky_ceiling_height", "visibility", "wind_speed"]:
         label_df = pd.read_csv("/media/jose/SAMSUNG/noaa/" + str(noaa_station) + "/" + str(noaa_station) + "_" + label + ".csv")
@@ -35,13 +34,21 @@ for tceq_name, tceq_coord in coords.items():
         del label_df['committer']
 
         label_df['commit_date'] = pd.to_datetime(label_df['commit_date'], errors='coerce', format='%Y-%m-%d 00:00:00 +0000 UTC')
-        label_df = label_df[(label_df['commit_date'] >= first_day) & (label_df['commit_date'] <= last_day)]
+        label_df = label_df[label_df.commit_date.isin(tceq_station_df.Date.values)]
         label_df = label_df.sort_values(by=['commit_date'])
 
-        label_df = label_df[~label_df.commit_date.isin(missing_dates)]
+        assert len(label_df) <= len(tceq_station_df)
 
-        diff = len(tceq_station_df) - len(label_df)
+        if len(label_df) != 0:
+            start_diff = tceq_station_df.loc[tceq_station_df.Date == label_df.commit_date.values[0]].index[0]
+            end_diff = len(tceq_station_df) - start_diff - len(label_df)
+        else:
+            start_diff = len(tceq_station_df)
+            end_diff = 0
 
-        #label_arr = (['NaN'] * startlist(label_df['avg']) + (['NaN'] * diff)
+        label_arr = (['NaN'] * start_diff) + list(label_df['avg']) + (['NaN'] * end_diff)
 
-        #tceq_station_df[label] = label_arr
+        tceq_station_df[label] = label_arr
+
+    print(tceq_station_df[10:15])
+        
