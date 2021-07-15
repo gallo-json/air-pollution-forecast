16986e66aeff1b6cbcc7b47f373af5f705608254
gallo-json <gallojsantiago@gmail.com>
2021-07-15 17:52:16 -0500

    workinmg

--- data/requests/web-scrape/fetch_monthly_AQI_data.py
+++ data/requests/web-scrape/fetch_monthly_AQI_data.py
@@ -1,9 +1,26 @@
+import io
+import json
 import requests
 from bs4 import BeautifulSoup
 
+with open("data/coords.json") as a: coords = json.load(a)
 url = "https://www.tceq.texas.gov/cgi-bin/compliance/monops/peak_monthly.pl?override"
 
 r = requests.get(url)
 soup = BeautifulSoup(r.text, features='html.parser')
+f = io.StringIO(soup.prettify()).readlines()
 
-print(soup.prettify())
+for num, line in enumerate(f):
+    for region_name, _ in coords.items():
+        arr = []
+        if line.strip() == region_name:
+            if f[num + 7].strip().isdigit() or f[num + 7].strip() == 'NA':
+                start_idx = num + 7
+            elif f[num + 12].strip().isdigit() or f[num + 12].strip() == 'NA':
+                start_idx = num + 12
+
+            while f[start_idx].strip().isdigit() or f[start_idx].strip() == 'NA':
+                arr.append(int(f[start_idx]) if f[start_idx].strip().isdigit() else 'NA')
+                start_idx += 3
+            
+            print(region_name, arr)
