a546a27388275bed35ac40c5a84f22193e36c4ac
gallo-json <gallojsantiago@gmail.com>
2021-07-15 19:15:01 -0500

    still

--- data/requests/api/fetch_current_weather_AQI_data.go
+++ data/requests/api/fetch_current_weather_AQI_data.go
@@ -9,8 +9,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
+type AQI struct {
+	Key string `json:"WA_API"`
+}
+
 func main() {
-	fmt.Println("Hello")
+	jsonFile, err := os.Open("data/API_keys.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+}
+
+func call(lat int, long int) {
+	resp, err := http.Get("http://api.weatherapi.com/v1/current.json?key=55f31beac75e4bd09e7135959210303&q=London&aqi=yes")
+	if err != nil {
+		// handle error
+	}
+	fmt.Println(resp)
 }
