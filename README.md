f82502aa1bf2da78b98b203ccf6182b855e0124b
gallo-json <gallojsantiago@gmail.com>
2021-07-18 19:12:08 -0500

    made useful function

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 https://www.tceq.texas.gov/cgi-bin/compliance/monops/peak_monthly.pl?override
 
 https://www.airnow.gov/aqi/aqi-calculator/
+
+0.01 loss inverse transformed into ~4 AQI points
--- src/app.py
+++ src/app.py
@@ -7,19 +7,22 @@ from get_weather import weather_forecast
 
 weight_dir = 'weights/stations/'
 
-model = load_model(weight_dir + 'Atascocita C560/weights', custom_objects={'loss_mse_warmup': loss_mse_warmup})
-x_scaler = load(weight_dir + 'Atascocita C560/x-scaler.gz')
-y_scaler = load(weight_dir + 'Atascocita C560/y-scaler.gz')
+def forecast_AQI(station_name):
+    model = load_model(weight_dir + station_name + '/weights', custom_objects={'loss_mse_warmup': loss_mse_warmup})
+    x_scaler = load(weight_dir + station_name + '/x-scaler.gz')
+    y_scaler = load(weight_dir + station_name + '/y-scaler.gz')
 
-df = weather_forecast('Atascocita C560')
-x_data = df[['air_temp', 'dew_point_temp', 'sea_level_pressure', 'wind_speed', 'visibility']].values
+    df = weather_forecast(station_name)
+    x_data = df[['air_temp', 'dew_point_temp', 'sea_level_pressure', 'wind_speed', 'visibility']].values
 
-x = x_scaler.transform(x_data)
-x = expand_dims(x, axis=0)
+    x = x_scaler.transform(x_data)
+    x = expand_dims(x, axis=0)
 
-preds = model.predict(x)
+    preds = model.predict(x)
 
-y_pred_rescaled = y_scaler.inverse_transform(preds[0])
+    y_pred_rescaled = y_scaler.inverse_transform(preds[0]).ravel().astype(int)
+    y_pred_rescaled[0] = df.AQI.iloc[0]
 
-print(y_pred_rescaled)
-print(df)
+    return y_pred_rescaled
+
+print(forecast_AQI('Baytown Garth C1017'))
