5edc10d58e8712e814614ea9847e0835b2ae8fb0
gallo-json <gallojsantiago@gmail.com>
2021-07-21 20:39:07 -0500

    map

--- src/app.py
+++ src/app.py
@@ -1,2 +1,31 @@
 import streamlit as st
 import pydeck as pdk
+from forecast import load_data
+from pandas import DataFrame
+
+forecasts = load_data()
+
+def map():
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": 29,
+            "longitude": -95,
+            "zoom": 50,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=forecasts,
+                get_position=["longitude", "latitude"],
+                radius=100,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+        ]
+    ))
+
+map()
