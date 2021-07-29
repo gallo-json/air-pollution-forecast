2104238ff1d9601f40a96d2f47c089bd94724785
gallo-json <gallojsantiago@gmail.com>
2021-07-29 13:38:54 -0500

    reordering stuff

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from scipy.spatial import KDTree
-
+from datetime import datetime
 from forecast import coords_df, find_region, forecast_AQI
 
 import streamlit.components.v1 as components
 
+date_now = datetime.now()
 
 _component_func = components.declare_component(
     "my_component",
@@ -23,4 +24,15 @@ clicked_coords = my_component()
 if type(clicked_coords) is dict:
     tree = KDTree(coords_df[['latitude', 'longitude']].values)
     idx = int(tree.query([clicked_coords['lat'], clicked_coords['lng']])[1])
-    st.write(forecast_AQI(coords_df['region'].iloc[idx]))
+    preds = forecast_AQI(coords_df['region'].iloc[idx])
+
+    col1, col2, col3 = st.beta_columns(3)
+
+    with col1:
+        st.header(date_now.strftime("%b") + ' ' + date_now.strftime("%d"))
+
+    with col2:
+        st.header(date_now.strftime("%b") + ' ' + str(int(date_now.strftime("%d")) + 1))
+
+    with col3:
+        st.header(date_now.strftime("%b") + ' ' + str(int(date_now.strftime("%d")) + 2))
