af4fbd2f174f726964e0f5a29ed06f8416f82e72
gallo-json <gallojsantiago@gmail.com>
2021-07-31 09:44:59 -0500

    might need to use PIL because cv2 text is ugly

--- src/frontend/make_boxes.py
+++ src/frontend/make_boxes.py
@@ -11,61 +11,42 @@ colors = [
     (76, 1, 153)
 ]
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+def get_center(text, font_size, font_thickness):
+    text_size = cv2.getTextSize(text, font, font_size, font_thickness)[0]
+
+    return int(size[1] - text_size[0]) // 2, int(size[0] + text_size[1]) // 2
+
 def draw(aqi):
     img = np.zeros(size, np.uint8)
 
-    font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 4
-    font_thickness = 2
-
     if (aqi <= 50):
-        c = colors[0]
+        cv2.rectangle(img, (0, 0), size[:2], colors[0], 10000)
+
         text = [
-            "GOOD",
-            "Air quality is satisfactory.",
-            "It is safe to go outside."
+            # (text, font_size, font_thickness, y_pos from center)
+            ("GOOD", 4, 5, -200),
+            ("AQI", 2, 5, -100),
+            (str(aqi), 4, 5, 0),
+            ("Air quality is satisfactory.", 2, 2, 100),
+            ("It is safe to go outside.", 2, 2, 200)
         ]
+
+        for t in text:
+            x, y = get_center(t[0], t[1], t[2])
+            cv2.putText(img, t[0], (x, y + t[3]), font, t[1], (0, 0, 0), t[2])
     elif (aqi >= 51) and (aqi <= 100):
-        c = colors[1] 
-        text = [
-            "MODERATE",
-            "No restrictions for general public.",
-            "It is safe to go outside."
-        ]
+        cv2.rectangle(img, (0, 0), size[:2], colors[1], 10000)
     elif (aqi >= 101) and (aqi <= 150):
-        c = colors[2] 
-        text = [
-            "UNHEALTHY FOR SENSITIVE GROUPS",
-            "Sensitive groups may experience health effects.",
-            "General public may not be affected."
-        ]
+        cv2.rectangle(img, (0, 0), size[:2], colors[2], 10000)
     elif (aqi >= 151) and (aqi <= 200):
-        c = colors[3] 
-        text = [
-            "UNHEALTHY",
-            "Anyone may experience health effects.",
-            "It is NOT safe for the general public to go outside."
-        ]
+        cv2.rectangle(img, (0, 0), size[:2], colors[3], 10000)
     else:
-        c = colors[4] 
-        text = [
-            "VERY UNHEALTHY",
-            "Anyone may experience more serious health effects.",
-            "It is NOT safe for the general public to go outside."
-        ]
-
-    cv2.rectangle(img, (0, 0), size[:2], c, 10000)
-
-    text_sizes = []
-
-    for t in text:
-        text_size = cv2.getTextSize(t, font, font_scale, font_thickness)[0]
-
-        text_sizes.append([int(img.shape[1] - text_size[0]) // 2, int(img.shape[0] + text_size[1]) // 2])
-    
-    cv2.putText(img, header, (header_x, header_y), font, font_scale, (0,0,0), font_thickness)
+        cv2.rectangle(img, (0, 0), size[:2], colors[4], 10000)
 
     return img
 
-cv2.imshow('mat', draw(78))
+
+cv2.imshow('mat', draw(8))
 cv2.waitKey(0)
